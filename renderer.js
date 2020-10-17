34a15d45e80c7d6edaf28f49309dcf00960963bf
dhanyn10 <dhanyn.dhan@gmail.com>
2020-10-17 13:12:54 +0700

    init

---
+++ renderer.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+const shell = require('electron').shell
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+document.getElementById('authorize').addEventListener('click', function (){
+    // Load client secrets from a local file.
+    fs.readFile('credentials.json', (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        // Authorize a client with credentials, then call the Google Drive API.
+        authorize(JSON.parse(content), listFiles);
+    });
+})
+
+function authorize(credentials, callback) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getAccessToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
+}
+
+function getAccessToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    shell.openExternal(authUrl)
+    document.getElementById('key').addEventListener('keypress', function (e) {
+        if(e.keyCode == 13)
+        {
+            var code = this.value
+            oAuth2Client.getToken(code, (err, token) => {
+                if (err) return console.error('Error retrieving access token', err);
+                oAuth2Client.setCredentials(token);
+                // Store the token to disk for later program executions
+                fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                    if (err) return console.error(err);
+                    console.log('Token stored to', TOKEN_PATH);
+                });
+                callback(oAuth2Client);
+            });
+        }
+    })
+}
+
+function listFiles(auth) {
+    const drive = google.drive({version: 'v3', auth});
+    drive.files.list({
+        q: "'root' in parents and mimeType='application/vnd.google-apps.folder'",
+        fields: '*',
+        spaces: 'drive',
+        fields: 'nextPageToken, files(id, name)',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const files = res.data.files;
+        if (files.length) {
+            console.log('My Drive(root):');
+            files.map((file) => {
+                console.log(`${file.name} (${file.id})`);
+            });
+        } else {
+            console.log('No files found.');
+        }
+    });
+}
