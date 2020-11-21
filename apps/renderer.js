9465ef20518bdee5a02576cb51e597d97d0482cf
dhanyn10 <dhanyn.dhan@gmail.com>
2020-11-22 00:03:26 +0700

    move apps script to /apps

--- apps/renderer.js
+++ apps/renderer.js
@@ -15,12 +15,12 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.metadata']
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json'
+const TOKEN_PATH = './apps/token.json'
 
 document.getElementById('authorize').addEventListener('click', function (){
     arrparents = []
     // Load client secrets from a local file.
-    fs.readFile('credentials.json', (err, content) => {
+    fs.readFile('./apps/credentials.json', (err, content) => {
         if (err)
             return console.log('Error loading client secret file:', err)
         // Authorize a client with credentials, then call the Google Drive API.
