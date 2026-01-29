13d3a306aa3b42807e4f1a99e4c7a2a75566296d
dhanyn10 <dhanyn.dhan@gmail.com>
2026-01-29 08:32:39 +0700

    update ci

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['Ubuntu Mono', 'monospace'],
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
