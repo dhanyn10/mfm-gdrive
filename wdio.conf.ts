f32701467a4c57cc1f02ee41458eb5aa76e01c44
dhanyn10 <dhanyn.dhan@gmail.com>
2026-01-29 08:02:43 +0700

    update ci

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -55,7 +55,7 @@ export const config: WebdriverIO.Config = {
     capabilities: [{
         browserName: 'electron',
         'wdio:electronServiceOptions': {
-            appArgs: ['--user-data-dir=/tmp/wdio-e2e-test-run']
+            appArgs: ['--no-sandbox', '--disable-dev-shm-usage', '--user-data-dir=/tmp/wdio-e2e-test-run']
         },
     }],
 
@@ -106,11 +106,9 @@ export const config: WebdriverIO.Config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [[
-        'electron', {
-            xvfb: true,
-        }
-    ]],
+    services: [
+        'electron'
+    ],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
