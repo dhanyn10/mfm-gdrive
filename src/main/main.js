de9688ed565226b105d179adba40656719035ffe
dhanyn10 <11478316+dhanyn10@users.noreply.github.com>
2026-03-20 02:56:28 +0000

    refactor: prefer top-level await pattern

--- src/main/main.js
+++ src/main/main.js
@@ -67,7 +67,9 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+(async () => {
+    await app.whenReady();
+
     // IPC handler to provide the user data path to the renderer process.
     // This is used for storing persistent data like authentication tokens.
     ipcMain.handle('get-user-data-path', () => {
@@ -117,7 +119,7 @@ app.whenReady().then(() => {
         // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
-});
+})();
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
